39c1089faa06d2b65ab642b2f725ff640aeac427
Taha <110016145+Tahacollege@users.noreply.github.com>
2024-07-24 20:27:30 +0530

    database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const MongoCLient=require('mongodb').MongoClient
-const url='mongodb://localhost:27017'
-//const url="mongodb+srv://tcollegewala30:fOcy87YhffoTWgnJ@cluster0.qqtdpgf.mongodb.net/b.net/"
+//const url='mongodb://localhost:27017'
+const url="mongodb+srv://tcollegewala30:fOcy87YhffoTWgnJ@cluster0.qqtdpgf.mongodb.net/b.net/"
 const database="jobportal"
 
 async function DbConnection(){
